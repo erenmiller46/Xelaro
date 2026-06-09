6322d9f19064114e9e074a306d5eb0eccce1074d
erenmiller46 <erenmiller46@gmail.com>
2026-06-09 20:27:53 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "I Am Watching you.",
+        name: "Four steps ahead of you.",
         // Activity type number (0 = Playing).
         type: 0, 
       },
